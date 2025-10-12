813748f599a0a008c0ac8b81350ee8a950b3d555
Elif Nur Beycan <162906315+elifnurbeycan@users.noreply.github.com>
2025-10-12 18:33:36 +0300

    Create playerMovement_HorizontalVertical.cs

---
+++ hafta1/scripts/playerMovement_HorizontalVertical.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerMovement_2DAxes : MonoBehaviour
+{
+    public float speed = 3f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");        // ←/→ veya A/D
+        float v = Input.GetAxis("Vertical");          // ↑/↓ veya W/S
+        Vector3 move = new Vector3(h, v, 0f);         // X ve Y eksenleri
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
+    }
+}
