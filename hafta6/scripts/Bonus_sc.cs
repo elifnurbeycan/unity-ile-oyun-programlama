b923d0a4d68621611ab85664b0af8cec2c198bee
Elif Nur Beycan <162906315+elifnurbeycan@users.noreply.github.com>
2025-11-14 12:11:14 +0300

    Add files via upload

---
+++ hafta6/scripts/Bonus_sc.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Bonus_sc : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 3;
+
+    [SerializeField]
+    int bonusId;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (this.transform.position.y < -5.8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            // Üçlü atış bonusunu aktifleştir
+            Player_sc player_sc = other.transform.GetComponent<Player_sc>();
+            if (player_sc != null)
+            {
+                switch (bonusId)
+                {
+                    // 0 Üçlü atış bonus'unu temsil eder
+                    case 0:
+                        player_sc.TripleShotActive();
+                        break;
+                    // 1 Hız bonus'unu temsil eder
+                    case 1:
+                        player_sc.SpeedBonusActive();
+                        break;
+                    // 2 Kalkan bonus'unu temsil eder
+                    case 2:
+                        Debug.Log("Kalkan bonus'u aktif");
+                        break;
+                    // Yalnızca 3 tane tanımladık. Bunların dışındakiler hata durumu!
+                    default:
+                        Debug.Log("Hata Durumu!");
+                        break;
+                }
+            }
+            // Bonus nesnesini yok et
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ hafta6/scripts/Enemy_sc.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Enemy_sc : MonoBehaviour
+{
+    [SerializeField]
+    int speed = 4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (this.transform.position.y < -5.5f)
+        {
+            this.transform.position = new Vector3(Random.Range(-9.5f, 9.5f),
+                                                    7.4f,
+                                                    0);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Çarpışma: " + other.tag);
+
+        if (other.tag == "Player")
+        {
+            //Player'ın canını bir eksilt
+            Player_sc player_sc = other.transform.GetComponent<Player_sc>();
+            player_sc.Damage();
+
+            Destroy(this.gameObject);
+        }
+        else if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
